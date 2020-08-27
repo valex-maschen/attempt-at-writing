e0c5c5f8bd39f0f628ff5b37fac2574b003feaea
valex-maschen <vaprikin@mail.ru>
2020-08-28 01:15:42 +0400

    2nd-commit

---
+++ landing-js.js
@@ -0,0 +1,8 @@
+const goToTop = document.querySelector("#goToTop");
+goToTop.addEventListener("click", function () {
+    window.scrollTo({
+      top: 0,
+      left: 0,
+      behavior: "smooth"
+    });
+});
